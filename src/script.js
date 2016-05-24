a8b760fa59d796a1ad41b05ce8aac60c24d3d249
Ethan Hartman <ethan@elhartman.com>
2016-05-24 10:02:29 -0400

    third wave

--- src/script.js
+++ src/script.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
-	var wid = prompt("Enter a size for your workspace (400,500,600)");
+	var wid = prompt("Enter a size for your workspace (divisible by 100)");
 	for (widStart=1;widStart <= wid;widStart++){
 		$('.contain').append('<div class="etchbox"></div>');
 	};
+	$('.etchbox').hover(function(){
+		$(this).css("background-color","#111");
+	});
+	$('#reset').click(function(){
+		$('.etchbox').css("background-color","#FFF")
+	});
+
 });
